0cd64f78d03d6c5282726b08404271256606020d
Kurlaev Nikita <yakuri2006@gmail.com>
2023-10-05 09:05:15 +0300

    Initial Commit

---
+++ recipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  recipes
+//
+//  Created by yakuri354 on 05.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ recipes/recipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipesApp.swift
+//  recipes
+//
+//  Created by yakuri354 on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct recipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
